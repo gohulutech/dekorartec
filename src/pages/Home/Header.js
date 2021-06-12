9b3bb271ac92c007321451ed51427267ba7d9f91
Hugo Arevalo <hugoarevalo@Hugos-MacBook-Pro.local>
2021-06-11 21:13:25 -0500

    Created template from Home

---
+++ src/pages/Home/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Dekorartec</h1>
+      <div class="ui three column grid">
+        <div class="column">
+          <div class="ui segment">
+            <img />
+          </div>
+        </div>
+        <div class="column">
+          <div class="ui segment">
+            <img />
+          </div>
+        </div>
+        <div class="column">
+          <div class="ui segment">
+            <img />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+const Home = () => {
+  return (
+    <div class="ui container">
+      <Header />
+
+      <button class="ui small blue button">Hello</button>
+    </div>
+  );
+};
+
+export default Home;
